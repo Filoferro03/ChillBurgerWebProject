1649ca56f2640ab7dc4dc52d4b4b24143b4a8f70
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-04-17 15:25:06 +0200

    improving the login-form page

--- chillburger/template/login-form.php
+++ chillburger/template/login-form.php
@@ -1,45 +1,46 @@
-
 <section class="">
     <div class="">
         <div class="">
             <div class="d-flex flex-column align-items-center">
-                <h2>Login</h2>
+                <form action="#" method="POST" class="d-flex flex-column">
+                    <fieldset class=" p-5 m-5 border border-dark rounded">
+                        <div class="text-center">
+                            <legend>Login</legend>
+                        </div>
+                        <label for="loginusername" class="form-label m-1">Username</label>
+                        <input type="text" id="loginusername" class="form-control m-1">
+                        <label for="loginpassword" class="m-1">Password</label>
+                        <input type="password" id="loginpassword" class="form-control m-1">
+                        <div class="text-center">
+                            <input type="submit" value="Login" class="m-2">
+                        </div>
+                    </fieldset>
+                </form>
             </div>
         </div>
-        <div>
-            <form action="#" method="POST">
-                <label for="name">Nome</label>
-                <input type="text" name="nome" id="name">
-                <label for="surname">Cognome</label>
-                <input type="text" id="surname">
-                <label for="registerpassword">Password</label>
-                <input type="text" id="registerpassword">
-                <label for="confirmpassword"> Conferma Password</label>
-                <input type="text" id="confirmpassword">
-                <button type="submit">Registrati</button>
+        <div class="d-flex flex-column align-items-center">
+            <form action="#" method="POST" class="d-flex flex-column w-70">
+                <fieldset class="p-5 m-5 border border-dark rounded">
+                    <div class="text-center">
+                        <legend>Registrazione</legend>
+                    </div>
+                    <label for="nome" class="form-label m-1 ">Nome</label>
+                    <input type="text" id="nome" class="form-control m-1">
+                    <label for="cognome">Cognome</label>
+                    <input type="text" id="cognome" class="form-control m-1">
+                    <label for="username" class="form-label m-1">Username</label>
+                    <input type="text" id="username" class="form-control m-1">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" class="form-control m-1">
+                    <label for="confermapassword">Conferma Password</label>
+                    <input type="password" id="confermapassword" class="form-control m-1">
+                    <div class="text-center">
+                        <input type="submit" value="Registrati" class="m-1">
+                    </div>
+
+                </fieldset>
             </form>
 
         </div>
     </div>
 </section>
-
-
-<?php
-/*
-  <form action="#" method="POST">
-                <label for="loginusername">Username</label>
-                <input type="text" id="loginusername">
-                <label for="loginpassword">Password</label>
-                <input type="password" id="loginpassword">
-                <button type="submit">Login</button>
-            </form>
-
-            <form action="#" method="POST">
-                        <label for="loginusername">Username</label>
-                        <input type="text" id="loginusername">
-                        <label for="loginpassword">Password</label>
-                        <input type="password" id="loginpassword">
-                        <button type="submit">Login</button>
-                    </form>  
- */
-?>
