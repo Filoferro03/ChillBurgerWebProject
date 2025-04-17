4c837baaac9eda72752b06dbba081ef4c382f038
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-04-17 13:00:42 +0200

    created the php files for the login

---
+++ chillburger/login.php
@@ -0,0 +1,9 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["nome"] = "template/login-form.php";
+$templateParams["titolo"] = "ChillBurger - Login";
+
+require("template/base.php")
+
+    ?>
---
+++ chillburger/template/login-form.php
@@ -0,0 +1,45 @@
+
+<section class="">
+    <div class="">
+        <div class="">
+            <div class="d-flex flex-column align-items-center">
+                <h2>Login</h2>
+            </div>
+        </div>
+        <div>
+            <form action="#" method="POST">
+                <label for="name">Nome</label>
+                <input type="text" name="nome" id="name">
+                <label for="surname">Cognome</label>
+                <input type="text" id="surname">
+                <label for="registerpassword">Password</label>
+                <input type="text" id="registerpassword">
+                <label for="confirmpassword"> Conferma Password</label>
+                <input type="text" id="confirmpassword">
+                <button type="submit">Registrati</button>
+            </form>
+
+        </div>
+    </div>
+</section>
+
+
+<?php
+/*
+  <form action="#" method="POST">
+                <label for="loginusername">Username</label>
+                <input type="text" id="loginusername">
+                <label for="loginpassword">Password</label>
+                <input type="password" id="loginpassword">
+                <button type="submit">Login</button>
+            </form>
+
+            <form action="#" method="POST">
+                        <label for="loginusername">Username</label>
+                        <input type="text" id="loginusername">
+                        <label for="loginpassword">Password</label>
+                        <input type="password" id="loginpassword">
+                        <button type="submit">Login</button>
+                    </form>  
+ */
+?>
