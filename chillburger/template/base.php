b9e4318b67f44f5d385634081af167c56e48b697
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2025-04-19 13:38:39 +0200

    minor fixes in dimentions

--- chillburger/template/base.php
+++ chillburger/template/base.php
@@ -76,21 +76,21 @@
                 <a href="index.php"><img src="./resources/ChillBurgerLogo.png" alt="ChillBurger Logo" class="logo" /></a>
                 <ul class="list-group list-group-horizontal d-flex justify-content-center gap-4">
                     <li class="list-group-item">
-                        <a class="nav-link header-menu fs-3" aria-current="page" href="#">Menu</a>
+                        <a class="nav-link header-menu fs-2" aria-current="page" href="#">Menu</a>
                     </li>
                     <li class="list-group-item">
-                        <a class="nav-link header-menu fs-3" href="#">Ordina ora</a>
+                        <a class="nav-link header-menu fs-2" href="#">Ordina ora</a>
                     </li>
                     <li class="list-group-item">
-                        <a class="nav-link header-menu fs-3" href="#">About us</a>
+                        <a class="nav-link header-menu fs-2" href="#">About us</a>
                     </li>
                     <li class="list-group-item">
-                        <a class="nav-link header-menu fs-3" href="reviews.php">Recensioni</a>
+                        <a class="nav-link header-menu fs-2" href="reviews.php">Recensioni</a>
                     </li>
                 </ul>
                 <div>
                     <a class="text-decoration-none" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="black"
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black"
                             class="bi bi-person-circle" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                             <path fill-rule="evenodd"
@@ -98,7 +98,7 @@
                         </svg>
                     </a>
                     <a href="#" class="text-decoration-none mx-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="black" class="bi bi-bell"
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" class="bi bi-bell"
                             viewBox="0 0 16 16">
                             <path
                                 d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6" />
@@ -110,7 +110,7 @@
     </header>
 
     <main>
-        <?php /*require($templateParams["nome"]);*/ require("template/homepage.php") ?>
+        <?php require($templateParams["nome"]); ?>
     </main>
 
     <footer class="py-3">
