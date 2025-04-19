b3bef1bf773f0130c5923294752fd9416f42066d
Riccardo Balzani <riccardobalzani@Air-di-Riccardo.homenet.telecomitalia.it>
2025-04-19 02:12:09 +0200

    small aestetich changes

--- chillburger/index.php
+++ chillburger/index.php
@@ -1,8 +1,18 @@
 <?php
+/* 
+    
+    FIXME
+    Per vedere gli errori nel browser, va tolto quando finiamo
+    
+*/
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once("bootstrap.php");
 
 $templateParams["titolo"] = "ChillBurger - Home";
 $templateParams["nome"] = "template/homepage.php";
 
-require("template/base.php")
+require("template/base.php");
 ?>
