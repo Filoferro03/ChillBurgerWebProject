e119f2875cd3df4f1378ed7bdea0fbef0f29995c
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2025-04-19 18:50:52 +0200

    started rwviews page js and style sheet

--- chillburger/reviews.php
+++ chillburger/reviews.php
@@ -3,6 +3,7 @@ require_once("bootstrap.php");
 
 $templateParams["titolo"] = "ChillBurger - Recensioni";
 $templateParams["nome"] = "reviews-list.php";
+$templateParams["css"] = ["css/reviews.css"];
 
 require("template/base.php")
 ?>
---
+++ chillburger/template/reviews-list.php
@@ -0,0 +1,5 @@
+<section class= "container-md m-auto py-4 text-center">
+<h2>Recensioni</h2>
+<p> Leggi le recensioni dei nostri clienti e scopri cosa pensano di noi!</p>
+<div class="accordion m-auto"> </div>
+</section>
