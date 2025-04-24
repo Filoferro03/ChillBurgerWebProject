f45e470187d563ba1c9fb420bc2f56a378a0fa2c
Riccardo Balzani <riccardobalzani@MacBook-Air-di-Riccardo.local>
2025-04-24 21:16:05 +0200

    menu done with a js script

--- chillburger/menu.php
+++ chillburger/menu.php
@@ -5,6 +5,7 @@ $templateParams = [
     "titolo" => "ChillBurger – Menu",
     "nome"   => "template/menu_main.php",
     "css"    => ["css/menu_style.css"],
+    "js"     => ["js/menu_filter.js"]
 ];
 
 require "template/base.php";
