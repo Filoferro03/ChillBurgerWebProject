deab619f4dc742ff5f99198e5d6f44a85b3b9f37
Riccardo Balzani <riccardobalzani@MacBook-Air-di-Riccardo.local>
2025-04-25 08:02:50 +0200

    manager page began

--- chillburger/template/manager_main.php
+++ chillburger/template/manager_main.php
@@ -2,29 +2,32 @@
 
 <section class="container py-5">
 
-    <!-- Titolo principale  -->
+    <!-- Titolo principale -->
     <h2 class="display-4 custom-title text-center mb-3">
-        <span class="txt">Manager </span>
+        <span class="txt">Manager</span>
         <span class="emoji">🧑🏼‍💻</span>
     </h2>
 
-    <!-- sezione pulsanti manager -->
+    <!-- Sezione pulsanti manager -->
     <div class="row justify-content-center mb-5">
         <div class="col-12 col-md-8 col-lg-6">
             <div class="card shadow-sm p-4 text-center manager-card">
-
-                <div class="d-flex flex-wrap justify-content-center gap-3">
-                    <a href="orders.php"        class="btn manager-btn">Orders</a>
-                    <a href="menu_admin.php"    class="btn manager-btn">Menu</a>
-                    <a href="stock.php"         class="btn manager-btn">Stock</a>
-                    <a href="notifications.php" class="btn manager-btn">Notification</a>
+                <div class="row row-cols-2 g-3">
+                    <div class="col">
+                        <a href="orders.php" class="btn manager-btn w-100">Orders</a>
+                    </div>
+                    <div class="col">
+                        <a href="menu_admin.php" class="btn manager-btn w-100">Menu</a>
+                    </div>
+                    <div class="col">
+                        <a href="stock.php" class="btn manager-btn w-100">Stock</a>
+                    </div>
+                    <div class="col">
+                        <a href="notifications.php" class="btn manager-btn w-100">Notification</a>
+                    </div>
                 </div>
-
             </div>
         </div>
     </div>
 
-
-
-    
 </section>
