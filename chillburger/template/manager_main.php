a8e9191be24533728071d4cef762ab00e0ee1b58
Riccardo Balzani <riccardobalzani@MacBook-Air-di-Riccardo.local>
2025-04-25 10:18:53 +0200

    manager buttons bigger

--- chillburger/template/manager_main.php
+++ chillburger/template/manager_main.php
@@ -14,16 +14,16 @@
             <div class="card shadow-sm p-4 text-center bg-dark">
                 <div class="row row-cols-2 g-3">
                     <div class="col">
-                        <a href="orders.php" class="btn  a-underline light">Orders</a>
+                        <a href="orders.php" class="h3 a-underline light">Orders</a>
                     </div>
                     <div class="col">
-                        <a href="menu_admin.php" class="btn  a-underline light">Menu</a>
+                        <a href="menu_admin.php" class="h3 a-underline light">Menu</a>
                     </div>
                     <div class="col">
-                        <a href="stock.php" class="btn  a-underline light">Stock</a>
+                        <a href="stock.php" class="h3 a-underline light">Stock</a>
                     </div>
                     <div class="col">
-                        <a href="notifications.php" class="btn  a-underline light">Notification</a>
+                        <a href="notifications.php" class="h3 a-underline light">Notification</a>
                     </div>
                 </div>
             </div>
