37414e0337ac59ee274e1a86ae6deaa7793d9c2e
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-04-26 19:31:58 +0200

    created the logout function

--- chillburger/utils/functions.php
+++ chillburger/utils/functions.php
@@ -4,12 +4,20 @@
 function registerLogin($user)
 {
     $_SESSION["username"] = $user["username"];
-    $_SESSION["type"] = $user["type"];
-    $_SESSION["name"] = $user["name"];
-    $_SESSION["surname"] = $user["surname"];
+    $_SESSION["tipo"] = $user["tipo"];
+    $_SESSION["nome"] = $user["nome"];
+    $_SESSION["cognome"] = $user["cognome"];
 }
 
 function isUserLoggedIn()
 {
     return !empty($_SESSION['username']);
+}
+
+function logout()
+{
+    unset($_SESSION["username"]);
+    unset($_SESSION["tipo"]);
+    unset($_SESSION["nome"]);
+    unset($_SESSION["cognome"]);
 }
