acf34184564ae5babc765e996919440375b5cad4
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2025-04-26 19:41:40 +0200

    created new page profile

---
+++ chillburger/profile.php
@@ -0,0 +1,8 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "ChillBurger - Profilo";
+$templateParams["nome"] = "profile-orders.php";
+
+require("template/base.php")
+?>
---
+++ chillburger/template/profile-orders.php
@@ -0,0 +1,3 @@
+<section>
+    <a href="index.php" class="btn btn-danger" onclick="logout()">Logout</a>
+</section>
