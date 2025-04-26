4b446692d1a2f4bbe95cc5d25b17711b7dac3bfd
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-04-26 19:27:29 +0200

    removed useless line in bootstrap php file

--- chillburger/bootstrap.php
+++ chillburger/bootstrap.php
@@ -3,5 +3,4 @@ session_start();
 define("RESOURCES_DIR", "./resources/");
 require_once("utils/functions.php");
 require_once("db/database.php");
-require_once("utils/functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "chillburgerdb", 3307);
