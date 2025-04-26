f0592dc39f06f32ee11c7a650ae1940421277a57
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2025-04-26 10:55:09 +0200

    modified header and linked to login.php

--- chillburger/bootstrap.php
+++ chillburger/bootstrap.php
@@ -2,4 +2,5 @@
 session_start();
 define("RESOURCES_DIR", "./resources/");
 require_once("db/database.php");
+require_once("utils/functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "chillburgerdb", 3307);
