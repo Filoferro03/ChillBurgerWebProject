e14e63b7672cb91e11ee361fc81d5c8de2b9fb09
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-05-06 18:44:52 +0200

    recreated some deleted files and added some pictures

--- chillburger/template/menu-main.php
+++ chillburger/template/menu-main.php
@@ -8,28 +8,42 @@
     </h2>
 
     <div class="row mb-3" id="appetizers">
-        <h1 class="text-center col-12">Antipasti</h1>
-
+        <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
+            <img src="./resources/menu/appetizers-bg.png" alt="Appetizers background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
+            <h1 class="text-center col-12 position-relative text-white" style="z-index: 1;">Antipasti</h1>
+        </div>
     </div>
+
 
     <div class="row mb-3" id="burgers">
-        <h1 class="text-center col-12">Panini</h1>
-
+        <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
+            <img src="./resources/menu/burgers-bg.png" alt="Burgers background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
+            <h1 class="text-center col-12 position-relative text-white" style="z-index: 1;">Panini</h1>
+        </div>
     </div>
+
 
     <div class="row mb-3" id="deepFried">
-        <h1 class="text-center col-12">Fritti</h1>
-
+        <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
+            <img src="./resources/menu/deep-fried-bg.png" alt="Deep Fried background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
+            <h1 class="text-center col-12 position-relative text-white" style="z-index: 1;">Fritti</h1>
+        </div>
     </div>
+
 
     <div class="row mb-3" id="drinks">
-        <h1 class="text-center col-12">Bevande</h1>
-
+        <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
+            <img src="./resources/menu/drinks-bg.png" alt="Drinks background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
+            <h1 class="text-center col-12 position-relative text-white" style="z-index: 1;">Bevande</h1>
+        </div>
     </div>
 
-    <div class="row mb-3" id="desserts">
-        <h1 class="text-center col-12">Dessert</h1>
 
+    <div class="row mb-3" id="desserts">
+        <div class="position-relative p-0 mb-5 d-flex align-items-center justify-content-center" style="height: 200px;">
+            <img src="./resources/menu/dessert-bg.png" alt="Dessert background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0 " style="z-index: 0;">
+            <h1 class="text-center col-12 position-relative text-white text-center" style="z-index: 1;">Dessert</h1>
+        </div>
     </div>
 
 </section>
---
+++ chillburger/template/menu_main.php
@@ -0,0 +1,74 @@
+<!-- template/menu_main.php -->
+<section class="container py-5">
+
+    <!-- Titolo principale -->
+    <h2 class="display-4 custom-title text-center mb-3">
+        <span class="txt">Il nostro menu</span>
+        <span class="emoji">🤤</span>
+    </h2>
+
+    <!-- Filtri opzionali -->
+    <div class="d-flex flex-wrap justify-content-center gap-2 mb-4">
+        <button class="btn btn-filter active" data-category="all">Tutto</button>
+        <button class="btn btn-filter" data-category="burger">Burger</button>
+        <button class="btn btn-filter" data-category="fries">Fries</button>
+        <button class="btn btn-filter" data-category="drink">Drink</button>
+    </div>
+
+    <!-- Lista prodotti -->
+    <div class="row g-4" id="menuGrid">
+
+        <!-- card prodotto -->
+        <?php
+
+        /* TODO:
+         *  
+         * array fittizio di prodotti (in futuro potrai caricarlo dal DB)
+         */
+        $prodotti = [
+            [
+                "cat" => "burger",
+                "img" => "./resources/menu/stock.jpg",
+                "nome" => "Classic Burger",
+                "desc" => "Manzo 150 g • Cheddar • Salsa house",
+                "prezzo" => "8,90 €"
+            ],
+            [
+                "cat" => "burger",
+                "img" => "./resources/menu/stock.jpg",
+                "nome" => "Double Smash",
+                "desc" => "Doppio manzo • Bacon croccante",
+                "prezzo" => "11,50 €"
+            ],
+            [
+                "cat" => "fries",
+                "img" => "./resources/menu/stock.jpg",
+                "nome" => "Patatine Rustiche",
+                "desc" => "Patate fresche con buccia",
+                "prezzo" => "3,50 €"
+            ],
+            [
+                "cat" => "drink",
+                "img" => "./resources/menu/stock.jpg",
+                "nome" => "Cola Bio 33 cl",
+                "desc" => "Dolcificata con zucchero di canna",
+                "prezzo" => "2,50 €"
+            ],
+        ];
+
+        foreach ($prodotti as $p): ?>
+            <div class="col-6 col-md-4 col-lg-3 menu-item" data-category="<?= $p['cat']; ?>">
+                <div class="card h-100 text-center shadow-sm hover-up">
+                    <img src="<?= $p['img']; ?>" class="card-img-top" alt="<?= $p['nome']; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $p['nome']; ?></h5>
+                        <p class="card-text small text-muted"><?= $p['desc']; ?></p>
+                    </div>
+                    <div class="card-footer bg-white border-0">
+                        <span class="fw-bold text-primary"><?= $p['prezzo']; ?></span>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
