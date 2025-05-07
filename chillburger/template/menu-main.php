90c2f4c50738d1a60143bd99c8b366e8fe220522
Riccardo Balzani <riccardobalzani@MacBook-Air-di-Riccardo.local>
2025-05-07 17:40:11 +0200

    small style changes

--- chillburger/template/menu-main.php
+++ chillburger/template/menu-main.php
@@ -1,5 +1,5 @@
 <!-- template/menu_main.php -->
-<section class="d-flex flex-column container">
+<section class="container py-5">
 
     <!-- Titolo principale -->
     <h2 class="display-4 custom-title text-center mb-3">
@@ -7,40 +7,45 @@
         <span class="emoji">🤤</span>
     </h2>
 
-    <div class="row mb-3" id="appetizers">
-        <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
-            <img src="./resources/menu/appetizers-bg.png" alt="Appetizers background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
+    <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center
+                    rounded-4 overflow-hidden"
+            style="height: 200px;">s="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
+            <img src="./resources/menu/antipasti.jpg" alt="Appetizers background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
             <h1 class="text-center col-12 position-relative text-white" style="z-index: 1;">Antipasti</h1>
         </div>
     </div>
 
 
-    <div class="row mb-3" id="burgers">
-        <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
-            <img src="./resources/menu/burgers-bg.png" alt="Burgers background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
+    <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center
+                    rounded-4 overflow-hidden"
+            style="height: 200px;">class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
+            <img src="./resources/menu/burgers.jpg" alt="Burgers background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
             <h1 class="text-center col-12 position-relative text-white" style="z-index: 1;">Panini</h1>
         </div>
     </div>
 
 
-    <div class="row mb-3" id="deepFried">
-        <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
+    <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center
+                    rounded-4 overflow-hidden"
+            style="height: 200px;"> class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
             <img src="./resources/menu/deep-fried-bg.png" alt="Deep Fried background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
             <h1 class="text-center col-12 position-relative text-white" style="z-index: 1;">Fritti</h1>
         </div>
     </div>
 
 
-    <div class="row mb-3" id="drinks">
-        <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
+    <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center
+                    rounded-4 overflow-hidden"
+            style="height: 200px;">lass="position-relative p-0 d-flex mb-5 align-items-center justify-content-center" style="height: 200px;">
             <img src="./resources/menu/drinks-bg.png" alt="Drinks background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0" style="z-index: 0;">
             <h1 class="text-center col-12 position-relative text-white" style="z-index: 1;">Bevande</h1>
         </div>
     </div>
 
 
-    <div class="row mb-3" id="desserts">
-        <div class="position-relative p-0 mb-5 d-flex align-items-center justify-content-center" style="height: 200px;">
+    <div class="position-relative p-0 d-flex mb-5 align-items-center justify-content-center
+                    rounded-4 overflow-hidden"
+            style="height: 200px;">class="position-relative p-0 mb-5 d-flex align-items-center justify-content-center" style="height: 200px;">
             <img src="./resources/menu/dessert-bg.png" alt="Dessert background" class="w-100 h-100 object-fit-cover position-absolute top-0 start-0 " style="z-index: 0;">
             <h1 class="text-center col-12 position-relative text-white text-center" style="z-index: 1;">Dessert</h1>
         </div>
