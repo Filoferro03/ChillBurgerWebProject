fc9239fb56dba3b4288158f8b80c24cd79af23e6
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-05-14 10:35:06 +0200

    removed some line

--- chillburger/js/cart.js
+++ chillburger/js/cart.js
@@ -31,32 +31,9 @@ async function getProductsInCart() {
             let result = "";
             let cartTotal=0.0;
             for(let i=0; i<products.length; i++) {
-                let totalProduct = products[i]["quantity"]*products[i]["price"];
-                cartTotal += totalProduct;
-                let html = `<div class="row text-center mx-2 my-2 p-3 cartElement">
-                    <div class='col-12 col-md-6'>
-                        <h3>${products[i]["name"]}</h3>
-                        <img src="./upload/${products[i]["image"]}" class="border rounded homeImage" alt="${products[i]["name"]}">
-                    </div>
-                    <div class = "col-12 col-md-6 align-items-center">
-                        <p class='mb-1 text-black'>Pezzi: ${products[i]["quantity"]}</p>
-                        <p class="fw-bold mb-1 text-black">${totalProduct}</p>
-                        <button class="btn btn-danger" data-id="${products[i]["idproduct"]}">Rimuovi</button>
-                    </div>
-                </div>`;
-                console.log(result);
-                result += html;
+                
             }
-            result += `<div class="d-grid gap-2 mx-2 p-3 d-flex justify-content-end text-light">
-                <h3>Prezzo totale: ${cartTotal}</h3>
-            </div>
-            <div class="d-grid gap-2 mx-2 p-3 d-flex justify-content-end text-light">
-            <span class="icons"><strong class="fa-brands fa-apple-pay fa-lg fa-fw"></strong></span>
-            <span class="icons"><strong class="fa-brands fa-cc-paypal fa-lg fa-fw"></strong></span>
-            <span class="icons"><strong class="fa-brands fa-cc-visa fa-lg fa-fw"></strong></span>
-            <span class="icons"><strong class="fa-brands fa-google-pay fa-lg fa-fw"></strong></span>
-            <button class='submit btn btn-large btn-dark' >Vai al Checkout</button>
-            </div>`
+            
             div.innerHTML = result ;
             const checkoutButton = document.querySelector("main > section > div > div:last-child button")
             console.log(checkoutButton);
