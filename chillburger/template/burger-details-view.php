2831d8fc9c1cc2bce1359339a91a5cbe23373d0e
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2025-05-14 11:17:21 +0200

    modified the view of the burger-details page

--- chillburger/template/burger-details-view.php
+++ chillburger/template/burger-details-view.php
@@ -1,5 +1,5 @@
 <section class="container d-flex flex-column align-items-center">
-    <div id="burger-details" class="d-flex flex-column p-2 m-2 align-items-center col-12 col-md-10">
+    <div id="burger-details" class="d-flex flex-column p-2 m-2">
 
     </div>
 </section>
