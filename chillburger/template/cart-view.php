6b91b62fdb3772437f48bfd92dce2c473b896e96
Riccardo Balzani <riccardobalzani@Air-di-Riccardo.homenet.telecomitalia.it>
2025-05-23 00:54:26 +0200

    small style changes

--- chillburger/template/cart-view.php
+++ chillburger/template/cart-view.php
@@ -1,5 +1,8 @@
-<section class="container d-flex flex-column align-items-center">
-    <h1>Carrello</h1>
+<section class="container py-5">
+    <h2 class="display-4 custom-title text-center mb-3">
+        <span class="txt">Carrello</span>
+        <span class="emoji">😋</span>
+    </h2>
     <div id="cart" class=" p-4 m-4 row w-100 ">
         <div id="cart-elements" class="d-flex row flex-column align-items-center ">
 
