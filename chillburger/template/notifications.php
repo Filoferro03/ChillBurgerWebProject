bb3d5f90201d389eed42bb38ecfe80798c57941f
Riccardo Balzani <riccardobalzani@Air-di-Riccardo.homenet.telecomitalia.it>
2025-05-23 01:11:45 +0200

    small style changes in notifications

--- chillburger/template/notifications.php
+++ chillburger/template/notifications.php
@@ -1,4 +1,9 @@
-<section>
+<section class="container py-5">
+    <h2 class="display-4 custom-title text-center mb-3">
+        <span class="txt">Notifiche</span>
+        <span class="emoji">🔔</span>
+    </h2>
+
     <div class=" m-3 p-3 w-[70%]">
         <div id="notes-container" class="d-flex flex-column items-center pt-2 pb-2 m-2">
 
