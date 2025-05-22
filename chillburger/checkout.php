4ba60209e190f9003ba04a360d09e4fd44e13a91
Filippo Ferretti <filippo.ferretti6@studio.unibo.it>
2025-05-22 16:07:34 +0200

    updated payment method

--- chillburger/checkout.php
+++ chillburger/checkout.php
@@ -3,8 +3,7 @@ require_once("bootstrap.php");
 
 $templateParams["titolo"] = "ChillBurger - Checkout";
 $templateParams["nome"] = "payment.php";
-$templateParams["css"] = array("css/reviews.css");
-$templateParams["js"] = array("js/reviews.js");
+$templateParams["js"] = array("js/checkout.js");
 
 require("template/base.php")
 ?>
