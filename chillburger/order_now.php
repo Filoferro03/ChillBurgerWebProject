b767c23ff27e5e74c0487a43e38747539426ea64
Riccardo Balzani <riccardobalzani@Air-di-Riccardo.homenet.telecomitalia.it>
2025-06-04 00:23:07 +0200

    acces denied for order now as admin added

--- chillburger/order_now.php
+++ chillburger/order_now.php
@@ -1,22 +1,40 @@
 <?php
-require_once "bootstrap.php";
+require_once("bootstrap.php");
 
-if (!isUserLoggedIn()) {
-    header("Location: login.php");
-    exit();
+// Inizializzo comunque l’array $templateParams in modo che sia sempre definito
+$templateParams = [];
+
+if (isUserLoggedIn()) {
+    if (isUserClient()) {
+        // Se è cliente carico il template “ordine ora”
+        $templateParams["nome"]   = "template/order_now_main.php";
+        $templateParams["titolo"] = "ChillBurger – Order Now";
+        $templateParams["css"]    = [
+          "css/style.css",
+          "css/menu_style.css",
+          "css/order_now_style.css"
+        ];
+        $templateParams["js"]     = [
+          "js/order_now.js"
+        ];
+    } else {
+        // Se è manager (o altro ruolo non cliente), mostro la pagina di “accesso negato”
+        $templateParams["nome"]   = "template/access_denied.php";
+        $templateParams["titolo"] = "Accesso Negato";
+        $templateParams["css"]    = [
+          "css/style.css"
+        ];
+    }
+} else {
+    // Non loggato: lo mando al login
+    $templateParams["nome"]   = "template/login-form.php";
+    $templateParams["titolo"] = "ChillBurger – Login";
+    $templateParams["css"]    = [
+      "css/style.css"
+    ];
+    $templateParams["js"]     = [
+      "js/login.js"
+    ];
 }
 
-$templateParams = [
-    "titolo" => "ChillBurger – Order_now",
-    "nome"   => "template/order_now_main.php",
-    "css"    => [
-        "css/style.css",
-        "css/menu_style.css",
-        "css/order_now_style.css"
-    ],
-    "js"     => [
-        "js/order_now.js"
-    ]
-];
-
-require "template/base.php";
+require("template/base.php");
---
+++ chillburger/template/access_denied.php
@@ -0,0 +1,20 @@
+<section class="container d-flex justify-content-center align-items-center min-vh-100">
+    <div class="row justify-content-center w-100">
+        <div class="col-12 col-md-8 col-lg-6">
+            <div class="card border-0 shadow-sm">
+                <div class="card-body text-center p-5">
+                    <h1 class="display-5 custom-title dark-brown-text mb-3">
+                    <span class="txt">ACCESSO NEGATO</span>
+                    <span class="emoji">🚫</span>
+                    </h1>
+                    <p class="h5 black-text mb-4">
+                    Per accedere a questa pagina è necessario un account di tipo cliente.
+                    </p>
+                    <a href="index.php" class="btn btn-body btn-lg">
+                    Torna alla Home
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
